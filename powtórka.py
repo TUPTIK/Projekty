2e08a01330b9387ef3bae3552bfc197c447a522f
TUPTIK <144586614+TUPTIK@users.noreply.github.com>
2024-02-12 07:28:27 +0100

    Create powtórka.py

---
+++ powtórka.py
@@ -0,0 +1,11 @@
+
+#zad1
+#   Zolnierzu jako niezabardzo ale jednak wykwalifikowany informatyk jesteś potrzebny swojemu 
+#kraju! Masz stworzyć system dla naszej fabryki czolgow. Przy urzyciu klasy masz nadawac karzdemu 
+#kolejnemu czolgowi(obiektowi) numer i musisz zaprogramowac informacje i ilości wyprodukowanych 
+#czolgow.
+
+#zad2
+#   Niestety ale nasza armia odniosla straty w czolgach. Potrzebujemy informacji o ilości sprawnych  
+#maszyn. Ztworz funkcje cls w ktorej podasz liczbe straconych maszyn i która zaktualizuje liczbe 
+#sprawnych (liczba wyprodukowanych maszyn ma byc nie tknienta).
